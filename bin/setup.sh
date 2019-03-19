999571361c9d3480de70267e8a42e7a58209cabb
Soypete <34897832+Soypete@users.noreply.github.com>
2019-03-19 15:49:03 -0600

    display wokring

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,5 +4,6 @@ go get github.com/kniren/gota/dataframe
 go get gonum.org/v1/plot/...
 go get github.com/sjwhitworth/golearn/knn
 go get github.com/sjwhitworth/golearn/base
+go get github.com/sjwhitworth/golearn/evaluation
 
 jupyter notebook 
